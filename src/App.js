29d6e1d30d04eb56a55707216c9e16c4845dbe4e
Eric Zhang <eric.zhang3@mail.mcgill.ca>
2023-01-02 19:42:53 -0500

    Changes by matthewflaherty

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+//
+//
+// export default App;
+
+import React from "react";
+import "./App.css";
+import SearchBar from "./Components/SearchBar";
+import NFLPlayerData from "./Data.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <SearchBar placeholder="Enter a Player Name..." data={NFLPlayerData} />
+
+      <table>
+          <tr>
+              <th>Name</th>
+              <th>Team</th>
+              <th>Division</th>
+              <th>Height</th>
+              <th>Age</th>
+              <th>Number</th>
+          </tr>
+          {/*{NFLPlayerData.map((val, key) => {*/}
+          {/*    return (*/}
+          {/*        <tr key={key}>*/}
+          {/*            <td>{val.name}</td>*/}
+          {/*            <td>{val.team}</td>*/}
+          {/*            <td>{val.div}</td>*/}
+          {/*            <td>{val.height}</td>*/}
+          {/*            <td>{val.age}</td>*/}
+          {/*            <td>{val.number}</td>*/}
+          {/*        </tr>*/}
+          {/*    )*/}
+          {/*})}*/}
+      </table>
+      </div>
   );
 }
 
